bc5221139db4e7c4025416ba0e4b079fcf7c7816
Nicha <nicha.wilanan@gmail.com>
2026-05-03 20:15:55 +0100

    feat: add tabs

---
+++ src/tabs/dine-out.tsx
@@ -0,0 +1,7 @@
+const DineOut = () => (
+  <div className="p-4 bg-blue-50 rounded-lg">
+    This is the DineOut component.
+  </div>
+);
+
+export default DineOut;
---
+++ src/tabs/outdoor.tsx
@@ -0,0 +1,7 @@
+const OutDoor = () => (
+  <div className="p-4 bg-blue-50 rounded-lg">
+    This is the OutDoor component.
+  </div>
+);
+
+export default OutDoor;
